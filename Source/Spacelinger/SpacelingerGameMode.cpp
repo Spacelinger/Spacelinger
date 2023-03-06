3c453b3c8f4a881c29e77d3e13f6984b79e387c6
Sergio Santana <src.santana@gmail.com>
2023-03-06 16:52:45 +0100

    Camera zoom while aiming

--- Source/Spacelinger/SpacelingerGameMode.cpp
+++ Source/Spacelinger/SpacelingerGameMode.cpp
@@ -7,7 +7,8 @@
 ASpacelingerGameMode::ASpacelingerGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Spacelinger/BP_ThirdPersonCharacter_Sergi"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
