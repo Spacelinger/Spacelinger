b680a11a30c110a7df2cf398a2562e4e6c6adee3
Sergio Santana <src.santana@gmail.com>
2023-04-02 19:20:04 +0200

    Moved things

--- Source/Spacelinger/SpacelingerGameMode.cpp
+++ Source/Spacelinger/SpacelingerGameMode.cpp
@@ -7,9 +7,11 @@
 ASpacelingerGameMode::ASpacelingerGameMode()
 {
 	// set default pawn class to our Blueprinted character
+	/*
+	NOTE(Sergi): We have a BluePrint to set the starting player character.
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	}*/
 }
