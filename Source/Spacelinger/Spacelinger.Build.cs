37d891881cbd56124157014869a9614e6e0bc96a
Sergio Santana <src.santana@gmail.com>
2023-04-03 19:08:10 +0200

    Added path to Build.cs

--- Source/Spacelinger/Spacelinger.Build.cs
+++ Source/Spacelinger/Spacelinger.Build.cs
@@ -8,6 +8,14 @@ public class Spacelinger : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"HeadMountedDisplay",
+			"EnhancedInput"
+		});
+		PublicIncludePaths.Add("Spacelinger");
 	}
 }
