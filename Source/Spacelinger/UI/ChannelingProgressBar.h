06695124743dce9a9da56dfdf07645276fc05277
DaniMonPeris (Gui) <rode.llino@gmail.com>
2023-06-21 18:29:19 +0200

    Fix

---
+++ Source/Spacelinger/UI/ChannelingProgressBar.h
@@ -0,0 +1,23 @@
+// Without Copyright. Project SpaceLinger, Master en Creacion de Videojuegos (MCV) at Universitat Pompeu Fabra (UPF)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ChannelingProgressBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACELINGER_API UChannelingProgressBar : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	//FORCEINLINE class UProgressBar* GetHealthBar() { return ChannelProgressBar; }
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* ChannelProgressBar;
+};
