9d00fe80a08a345bfb09a98268b18990bdc814a6
DaniMonPeris <daniel.montes@alum.upf.edu>
2023-09-06 19:18:21 +0200

    Credits + DeathMenu

---
+++ enc_temp_folder/f713ef1ca879a3e231842a3045f2681e/LifeComponent.cpp
@@ -0,0 +1,42 @@
+// Without Copyright. Project SpaceLinger, Master en Creacion de Videojuegos (MCV) at Universitat Pompeu Fabra (UPF)
+
+#pragma once
+
+#include "Components/LifeComponent.h"
+
+ULifeComponent::ULifeComponent() {
+
+}
+
+void ULifeComponent::InitializeComponent() {
+
+}
+
+void ULifeComponent::ReceiveDamage(int Damage, AActor* DamageDealer) {
+
+    CurrentLife = FMath::Max(CurrentLife - Damage, 0);
+    OnDamageReceivedDelegate.Broadcast(Damage, DamageDealer);
+
+    if (CurrentLife == 0) {
+        OnDie(DamageDealer);
+    }
+}
+
+/*
+void ULifeComponent::ReceiveHeal(int Heal, AActor* Healer) {
+
+    CurrentLife = FMath::Min(CurrentLife + Heal, MaxLife);
+    OnHealReceived.Broadcast(Heal, Healer);
+}
+*/
+
+void ULifeComponent::OnDie(AActor* Killer) {
+
+    OnDieDelegate.Broadcast(Killer);
+    GetWorld()->GetTimerManager().SetTimer(DestroyTimerHandle, this, &ULifeComponent::OnDestroyTimer, TimeToDestroyAfterDeath, false);
+}
+
+void ULifeComponent::OnDestroyTimer() {
+
+    GetOwner()->Destroy();
+}
