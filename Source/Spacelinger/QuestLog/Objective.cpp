30c83a2be729a9ef8d7392fab376f33548e6c87f
Rodellino <45183382+Rodellino@users.noreply.github.com>
2023-09-30 20:43:59 +0200

    Quest Log Updates - Map (almost) completed

---
+++ Source/Spacelinger/QuestLog/Objective.cpp
@@ -0,0 +1,9 @@
+// Without Copyright. Project SpaceLinger, Master en Creacion de Videojuegos (MCV) at Universitat Pompeu Fabra (UPF)
+
+
+#include "QuestLog/Objective.h"
+
+UObjective::UObjective()
+{
+}
+
