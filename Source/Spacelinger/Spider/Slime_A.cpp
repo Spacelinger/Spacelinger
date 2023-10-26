3a16b0692024ea374294e5ddc6f41ba0db0fb48c
Sergio Santana <src.santana@gmail.com>
2023-10-26 13:16:50 +0200

    Merge Dani

--- Source/Spacelinger/Spider/Slime_A.cpp
+++ Source/Spacelinger/Spider/Slime_A.cpp
@@ -846,14 +846,14 @@ void ASlime_A::CutSpiderWeb()
 		}
 
 		// Draw debug line to visualize the raycast in the editor (optional)
-		if (bHit)
+		/*if (bHit)
 		{
 			DrawDebugLine(GetWorld(), StartLocation, HitResult.Location, FColor::Green, false, 5.0f, 0, 1.0f);
 		}
 		else
 		{
 			DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 5.0f, 0, 1.0f);
-		}
+		}*/
 	}
 	attached = false;
 	attachedAtCeiling = false;
